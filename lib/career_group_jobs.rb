ea04a41002c399c1a24ff0f485ce5d6d57b0a005
rforgeon <rforgeon@gmail.com>
2017-08-02 18:31:31 +0100

    rubocop corrected

--- lib/career_group_jobs.rb
+++ lib/career_group_jobs.rb
@@ -1,79 +1,71 @@
 class CareerGroupJobs
-
-  #Use nokogiri parsing
+  # Use nokogiri parsing
   require 'nokogiri'
   require 'open-uri'
 
-
-  def initialize
-
-  end
+  def initialize; end
 
   def call(search)
     jobs = page_scrape(search)
-
-    scrape = jobs.map{|job| job_scrape(job)}
-    return scrape
+    scrape = jobs.map { |job| job_scrape(job) }
+    scrape
   end
 
-  #Scrape the url's of each job on the front page
+  # Scrape the url's of each job on the front page
   def page_scrape(search)
-
-    #Open first page of search
+    # Open first page of search
     page = Nokogiri::HTML(open("https://jobonline.thecareersgroup.co.uk/careersgroup/student/Vacancies.aspx?st=#{search}"))
     job_urls = []
-    #Loop through each job div on the page
+    # Loop through each job div on the page
     job_listings = 10
     job_listings.times do |i|
       job = page.css("#ctl00_ContentPlaceHolder1_SearchVacancies1_VacancySummaryUC1_lvVacancies_ctrl#{i}_pnlVacancy")
-      link = job.at_css("a")
-      job_url = "https://jobonline.thecareersgroup.co.uk/careersgroup/student/"+link["href"]
+      link = job.at_css('a')
+      job_url = 'https://jobonline.thecareersgroup.co.uk/careersgroup/student/' + link['href']
       job_urls << job_url
     end
-    return job_urls
+    job_urls
   end
 
-  #Scrape the details of each job from an array of URL's
+  # Scrape the details of each job from an array of URL's
   def job_scrape(url)
-    job ={}
+    job = {}
 
-      page = Nokogiri::HTML(open(url))
+    page = Nokogiri::HTML(open(url))
 
-      job_title = page.at_css(".v2_title").text.strip
+    job_title = page.at_css('.v2_title').text.strip
 
-      details = page.xpath("/html/body/div[@id='gradsIntoCareersWrapper']/div[@id='gradsIntoCareersContent']/div[@id='gradsIntoCareersCenter']/div[@id='centerContent']/form[@id='aspnetForm']/div[@id='v2_centreCol']/div[@id='ctl00_ContentPlaceHolder1_DisplayVacancyUC1_UpdatePanel1']/div[@id='ctl00_ContentPlaceHolder1_DisplayVacancyUC1_pnlRounded']").text.strip.split("\n")
+    details = page.xpath("/html/body/div[@id='gradsIntoCareersWrapper']/div[@id='gradsIntoCareersContent']/div[@id='gradsIntoCareersCenter']/div[@id='centerContent']/form[@id='aspnetForm']/div[@id='v2_centreCol']/div[@id='ctl00_ContentPlaceHolder1_DisplayVacancyUC1_UpdatePanel1']/div[@id='ctl00_ContentPlaceHolder1_DisplayVacancyUC1_pnlRounded']").text.strip.split("\n")
 
-      #Set the variables for each job detail
-      company = ''
-      wage = ''
-      location = ''
-      sector = ''
-      hours = ''
-      date_posted = ''
-      degree_level = ''
-      description = ''
+    # Set the variables for each job detail
+    company = ''
+    wage = ''
+    location = ''
+    sector = ''
+    hours = ''
+    date_posted = ''
+    degree_level = ''
+    description = ''
 
-      #Parse the exact text for each detail
-      details.each_with_index do |line,i|
-        stripped = line.strip
-        case stripped
-        when "Recruiter:"
-          company = details[i+1].strip
-        when "Salary:"
-          wage = details[i+1].strip
-        when "Location:"
-          location = details[i+1].strip
-        when "Hours:"
-          hours = details[i+1].strip
-        when "Date posted:"
-          date_posted = details[i+1].strip
-        when "Job description"
-          description = details[i+2].strip
-        else
-          #raise alert
-        end
+    # Parse the exact text for each detail
+    details.each_with_index do |line, i|
+      stripped = line.strip
+      case stripped
+      when 'Recruiter:'
+        company = details[i + 1].strip
+      when 'Salary:'
+        wage = details[i + 1].strip
+      when 'Location:'
+        location = details[i + 1].strip
+      when 'Hours:'
+        hours = details[i + 1].strip
+      when 'Date posted:'
+        date_posted = details[i + 1].strip
+      when 'Job description'
+        description = details[i + 2].strip
+      end
 
-      #Create a hash for the job
+      # Create a hash for the job
       job = {
         job_title: job_title,
         company: company,
@@ -83,16 +75,11 @@ class CareerGroupJobs
         hours: hours,
         date_posted: date_posted,
         degree_level: degree_level,
-        description: description,
+        description: description
       }
-
     end
 
-    #Return job
-    return job
-
+    # Return job
+    job
   end
-
-
-
 end
--- lib/tasks/jobs.rake
+++ lib/tasks/jobs.rake
@@ -1,26 +1,23 @@
-desc "Fetch jobs"
-task :fetch_jobs => :environment do
-
-# Task scrapes the front page jobs from https://jobonline.thecareersgroup.co.uk/careersgroup/student
-# Enter 'rake fetch_jobs <your search term>' to search for a keyword
+desc 'Fetch jobs'
+task fetch_jobs: :environment do
+  # Task scrapes the front page jobs from https://jobonline.thecareersgroup.co.uk/careersgroup/student
+  # Enter 'rake fetch_jobs <your search term>' to search for a keyword
 
   require 'career_group_jobs'
 
-  #Request a search perameter in terminal
+  # Request a search perameter in terminal
   puts "\n Enter search keyword(s): "
   search = STDIN.gets.chomp
 
-  #Notify the start of the page scrape including the search criteria entered
+  # Notify the start of the page scrape including the search criteria entered
   if search == ''
-    puts "Scraping Most Recent Jobs..."
+    puts 'Scraping Most Recent Jobs...'
   else
     puts "Scraping Jobs with keyword #{search}..."
   end
 
-  ####MAIN####
   career_group = CareerGroupJobs.new
-  #Scrape each job on the first page
+  # Scrape each job on the first page
   puts career_group.call(search).to_json
-  puts "...end of Scrape."
-
+  puts '...end of Scrape.'
 end
